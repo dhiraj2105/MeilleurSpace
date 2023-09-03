cff986bac2eae1d9cf63e2d27d0e51f41964ee15
Dhiraj <dkskp2005@gmail.com>
2023-09-03 17:08:05 +0530

    profile side

---
+++ src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProfileSide.css'
+
+function ProfileSide() {
+  return (
+    <div className='ProfileSide'>
+        ProfileSide
+    </div>
+  )
+}
+
+export default ProfileSide
