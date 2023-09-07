4ad2c2a1e9ad5319a98a4a6b725d8fa7b1c8de1f
Dhiraj <dkskp2005@gmail.com>
2023-09-07 16:04:16 +0530

    share modal updates

--- src/components/rightSide/RightSide.jsx
+++ src/components/rightSide/RightSide.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./RightSide.css";
 import Home from "../../img/home.png";
 import Noti from "../../img/noti.png";
 import Comment from "../../img/comment.png";
 import { UilSetting } from "@iconscout/react-unicons";
 import TrendCard from "../trendCard/TrendCard";
+import ShareModal from "../shareModal/ShareModal";
 
 function RightSide() {
+  const [modalOpened, setModalOpened] = useState(false);
+
   return (
     <div className="RightSide">
       <div className="navIcon">
@@ -18,7 +21,10 @@ function RightSide() {
 
       <TrendCard />
 
-      <button className="button r-button">Share</button>
+      <button className="button r-button" onClick={() => setModalOpened(true)}>
+        Share
+      </button>
+      <ShareModal modalOpened={modalOpened} setModalOpened={setModalOpened} />
     </div>
   );
 }
