1ee584f5309bfae5d42c53457ac63e92575792aa
Dhiraj <dkskp2005@gmail.com>
2023-09-13 21:09:12 +0530

    created post routes

---
+++ server/Routes/PostRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { createPost, getPost } from "../Controllers/PostController.js";
+
+const router = express.Router();
+
+router.post("/", createPost);
+router.get("/:id", getPost);
+
+export default router;
