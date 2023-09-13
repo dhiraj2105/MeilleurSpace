313db1454b572857995b493992b7d48a87644db1
Dhiraj <dkskp2005@gmail.com>
2023-09-13 21:08:26 +0530

    follow and unfollow user

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -59,3 +59,54 @@ export const deleteUser = async (req, res) => {
     res.status(403).json("Access Denied! you can only delete your own profile");
   }
 };
+
+//follow a user
+export const followUser = async (req, res) => {
+  const id = req.params.id;
+
+  const { currentUserId } = req.body;
+
+  if (currentUserId === id) {
+    res.status(403).json("Action forbidden");
+  } else {
+    try {
+      const followUser = await UserModel.findById(id);
+      const followingUser = await UserModel.findById(currentUserId);
+      if (!followUser.followers.includes(currentUserId)) {
+        await followUser.updateOne({ $push: { followers: currentUserId } });
+        await followingUser.updateOne({ $push: { following: id } });
+        res.status(200).json("User followed");
+      } else {
+        res.status(403).json("User is already followed by You");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+};
+
+//unfollow a user
+export const unfollowUser = async (req, res) => {
+  const id = req.params.id;
+
+  const { currentUserId } = req.body;
+
+  if (currentUserId === id) {
+    res.status(403).json("Action forbidden");
+  } else {
+    try {
+      const followUser = await UserModel.findById(id);
+      const followingUser = await UserModel.findById(currentUserId);
+
+      if (followUser.followers.includes(currentUserId)) {
+        await followUser.updateOne({ $pull: { followers: currentUserId } });
+        await followingUser.updateOne({ $pull: { following: id } });
+        res.status(200).json("User Unfollowed!");
+      } else {
+        res.status(403).json("User is not followed by you");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+};
