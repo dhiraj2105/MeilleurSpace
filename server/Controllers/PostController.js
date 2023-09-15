e47d6e5ac71a811faa9483d0e4e90b6229e24297
Dhiraj <dkskp2005@gmail.com>
2023-09-15 17:12:55 +0530

    update, delete, like dislike, get post timeline created

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -1,4 +1,5 @@
 import PostModel from "../Models/postModel.js";
+import UserModel from "../Models/userModel.js";
 import mongoose from "mongoose";
 
 //create new post
@@ -19,6 +20,98 @@ export const getPost = async (req, res) => {
   try {
     const post = await PostModel.findById(id);
     res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+//update post
+export const updatePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(postId);
+    if (post.userId === userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Post updated");
+    } else {
+      res.status(403).json("Action forbidden");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+//delete a post
+export const deletePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(postId);
+    if (post.userId == userId) {
+      await post.deleteOne();
+      res.status(200).json("Post deleted");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+//lke or dislike post
+export const likePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(postId);
+    if (!post.likes.includes(userId)) {
+      await post.updateOne({ $push: { likes: userId } });
+      res.status(200).json("Post liked");
+    } else {
+      await post.updateOne({ $pull: { likes: userId } });
+      res.status(200).json("Post Unliked");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+//get post's timeline
+export const getPostTimeline = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const currentUserPosts = await PostModel.find({ userId: userId });
+    const followingPosts = await UserModel.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $lookup: {
+          from: "posts",
+          localField: "following",
+          foreignField: "userId",
+          as: "followingPosts",
+        },
+      },
+      {
+        $project: {
+          followingPosts: 1,
+          _id: 0,
+        },
+      },
+    ]);
+    res.status(200).json(
+      currentUserPosts
+        .concat(...followingPosts[0].followingPosts)
+        .sort((a, b) => {
+          return b.createdAt - a.createdAt;
+        })
+    );
   } catch (error) {
     res.status(500).json(error);
   }
