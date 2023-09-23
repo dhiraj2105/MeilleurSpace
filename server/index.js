fe72b55552a244b8a2a50c8cc3ec315c328b28d1
Dhiraj <dkskp2005@gmail.com>
2023-09-23 18:47:47 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import AuthRoute from "./Routes/AuthRoute.js";
 import UserRoute from "./Routes/UserRoute.js";
 import PostRoute from "./Routes/PostRoute.js";
@@ -11,6 +12,8 @@ const app = express();
 // middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true })); //limit of json
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
 
 dotenv.config();
 
