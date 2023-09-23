0b77fab5a880c83340c6130efc803da65ac676e3
Dhiraj <dkskp2005@gmail.com>
2023-09-23 18:52:22 +0530

    dispatch and API added with authactions

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import "./Auth.css";
 import Logo from "../../img/logo.png";
+import { useDispatch, useSelector } from "react-redux";
+import { logIn, signUp } from "../../actions/AuthAction";
 
 function Auth() {
   const [isSignUp, setIsSignUp] = useState(true);
+
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.authReducer.loading);
+  console.log(loading);
 
   const [data, setData] = useState({
     firstname: "",
@@ -23,9 +29,11 @@ function Auth() {
     e.preventDefault();
 
     if (isSignUp) {
-      if (data.password !== data.confirmpassword) {
-        setConfirmPass(false);
-      }
+      data.password === data.confirmpassword
+        ? dispatch(signUp(data))
+        : setConfirmPass(false);
+    } else {
+      dispatch(logIn(data));
     }
   };
 
@@ -135,8 +143,12 @@ function Auth() {
                 : "Don't have an account? SignUp"}
             </span>
           </div>
-          <button className="button infoButton" type="submit">
-            {isSignUp ? "Signup" : "Log In"}
+          <button
+            className="button infoButton"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? "Loading..." : isSignUp ? "Signup" : "Log In"}
           </button>
         </form>
       </div>
