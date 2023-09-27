59e1d259af23cca258b5e8b06ee5705cf08575e6
Dhiraj <dkskp2005@gmail.com>
2023-09-27 19:24:18 +0530

    Routes created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./pages/auth/Auth";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import { Routes, Route, Navigate } from "react-router-dom";
+// import authReducer from "./reducers/authReducer";
+
 function App() {
+  const user = useSelector((state) => state.authReducer.authData);
   return (
     <div className="App">
       {/* blurs */}
       <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
 
-      {/* <Home /> */}
-      {/* <Profile /> */}
-      <Auth />
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
+        />
+        <Route
+          path="/home"
+          element={user ? <Home /> : <Navigate to="../auth" />}
+        />
+        <Route
+          path="/auth"
+          element={user ? <Navigate to="../home" /> : <Auth />}
+        />
+      </Routes>
     </div>
   );
 }
