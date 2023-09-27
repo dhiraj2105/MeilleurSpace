68c6c9406699dee1a15b5eb95f905dbeed608156
Dhiraj <dkskp2005@gmail.com>
2023-09-27 19:23:10 +0530

    upload Route added

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import cors from "cors";
 import AuthRoute from "./Routes/AuthRoute.js";
 import UserRoute from "./Routes/UserRoute.js";
 import PostRoute from "./Routes/PostRoute.js";
+import UploadRoute from "./Routes/UploadRoute.js";
 
 const app = express();
 
@@ -34,3 +35,4 @@ mongoose
 app.use("/auth", AuthRoute);
 app.use("/user", UserRoute);
 app.use("/post", PostRoute);
+app.use("/upload", UploadRoute);
