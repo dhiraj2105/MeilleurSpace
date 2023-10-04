3e13339ba961880dc5b317f54c788b9b69ef42c6
Dhiraj <dkskp2005@gmail.com>
2023-10-04 22:56:21 +0530

    user Request created

---
+++ src/api/UserRequest.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+const API = axios.create({ baseURL: "http://localhost:5000" });
+
+export const getUser = (userId) => API.get(`/user/${userId}`);
+export const updateUser = (id, fromData) => API.put(`/user/${id}`, fromData);
