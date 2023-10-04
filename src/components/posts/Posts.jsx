bd1d435c673ac3dafb879679aec52cad1827168a
Dhiraj <dkskp2005@gmail.com>
2023-10-04 22:59:02 +0530

    posts dynamic

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Posts.css";
-
-import { PostData } from "../../Data/postData";
 import Post from "../post/Post";
+import { useDispatch, useSelector } from "react-redux";
+import { getTimeLinePosts } from "../../actions/postAction";
+import { useParams } from "react-router-dom";
 
 function Posts() {
+  const params = useParams();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.authReducer.authData);
+  let { posts, loading } = useSelector((state) => state.postReducer);
+
+  useEffect(() => {
+    dispatch(getTimeLinePosts(user._id));
+  }, []);
+
+  if (!posts) return "No Posts";
+  if (params.id) posts = posts.filter((post) => post.userid === params.id);
   return (
     <div className="Post">
-      {PostData.map((post, id) => {
-        return <Post data={post} id={id} />;
-      })}
+      {loading
+        ? "Fetching Posts..."
+        : posts.map((post, id) => {
+            return <Post data={post} id={id} />;
+          })}
     </div>
   );
 }
