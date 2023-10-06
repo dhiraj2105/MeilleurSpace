12fca51990692ab07dbdf16592e46411bef7ec1e
Dhiraj <dkskp2005@gmail.com>
2023-10-06 09:55:32 +0530

    getAllUser api

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -20,6 +20,20 @@ export const getUser = async (req, res) => {
   }
 };
 
+//get all user
+export const getAllUser = async (req, res) => {
+  try {
+    let users = await UserModel.find();
+    users = users.map((user) => {
+      const { password, ...otherDetails } = user._doc;
+      return otherDetails;
+    });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 //update a user
 export const updateUser = async (req, res) => {
   const id = req.params.id;
