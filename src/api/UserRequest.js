47651256c13a9dab8a0579a05926b062c2f64cf3
Dhiraj <dkskp2005@gmail.com>
2023-10-06 09:56:33 +0530

    getAllUser api route

--- src/api/UserRequest.js
+++ src/api/UserRequest.js
@@ -3,3 +3,4 @@ const API = axios.create({ baseURL: "http://localhost:5000" });
 
 export const getUser = (userId) => API.get(`/user/${userId}`);
 export const updateUser = (id, fromData) => API.put(`/user/${id}`, fromData);
+export const getAllUser = () => API.get("/user");
