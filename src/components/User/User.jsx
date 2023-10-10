99e0f1c0a930c501028e61b3b5b783d23001df44
Dhiraj <dkskp2005@gmail.com>
2023-10-10 14:39:25 +0530

    follow unfolllow user function created

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { followUser, unFollowUser } from "../../actions/UserAction";
 
 function User({ person }) {
+  const dispatch = useDispatch();
+  const serverPublicFolder = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const [following, setFollowing] = useState(
+    person.followers.includes(user._id)
+  );
+
+  const handleFollow = () => {
+    following
+      ? dispatch(unFollowUser(person._id, user))
+      : dispatch(followUser(person._id, user));
+
+    setFollowing((prev) => !prev);
+  };
+
   return (
     <div className="follower">
       <div>
-        <img src={person.img} className="followerImg" />
+        <img
+          src={
+            person.coverPicture
+              ? serverPublicFolder + person.profilePicture
+              : serverPublicFolder + "defaultProfile.png"
+          }
+          className="followerImg"
+        />
         <div className="name">
-          <span>{person.name}</span>
+          <span>{person.firstname}</span>
           <span>@{person.username}</span>
         </div>
       </div>
-      <button className="button fc-button">Follow</button>
+      <button
+        className={
+          following ? "button fc-button UnFollowButton" : "button fc-button"
+        }
+        onClick={handleFollow}
+      >
+        {following ? "UnFollow" : "Follow"}
+      </button>
     </div>
   );
 }
