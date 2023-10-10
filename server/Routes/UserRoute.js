c8a62e9b1f3e68cfcc0ce09d657f7509c7ad17ad
Dhiraj <dkskp2005@gmail.com>
2023-10-10 14:35:24 +0530

    authMiddleware added for api routes

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -7,14 +7,15 @@ import {
   unfollowUser,
   updateUser,
 } from "../Controllers/UserController.js";
+import authMiddleWare from "../Middleware/authMiddleWare.js";
 
 const router = express.Router();
 
 router.get("/", getAllUser);
 router.get("/:id", getUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
-router.put("/:id/follow", followUser);
-router.put("/:id/unfollow", unfollowUser);
+router.put("/:id", authMiddleWare, updateUser);
+router.delete("/:id", authMiddleWare, deleteUser);
+router.put("/:id/follow", authMiddleWare, followUser);
+router.put("/:id/unfollow", authMiddleWare, unfollowUser);
 
 export default router;
