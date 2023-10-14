81d4f5f2e12b642f85ee4ceef14a7ec392aa11c0
Dhiraj <dkskp2005@gmail.com>
2023-10-15 00:13:20 +0530

    Route created for chat

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Auth from "./pages/auth/Auth";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import { Routes, Route, Navigate } from "react-router-dom";
-// import authReducer from "./reducers/authReducer";
+import Chat from "./pages/chat/Chat";
 
 function App() {
   const user = useSelector((state) => state.authReducer.authData);
@@ -30,6 +30,10 @@ function App() {
         <Route
           path="/profile/:id"
           element={user ? <Profile /> : <Navigate to="../auth" />}
+        />
+        <Route
+          path="/chat"
+          element={user ? <Chat /> : <Navigate to="../auth" />}
         />
       </Routes>
     </div>
