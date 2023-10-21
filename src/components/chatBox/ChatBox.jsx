edd19048348052f1fb53c7ca55f80e7fd709fd61
Dhiraj <dkskp2005@gmail.com>
2023-10-21 15:47:12 +0530

    message chat box  created

---
+++ src/components/chatBox/ChatBox.jsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import "./ChatBox.css";
+import { getUser } from "../../api/UserRequest";
+import { getMessages } from "../../api/MessageRequest";
+import { format } from "timeago.js";
+import InputEmoji from "react-input-emoji";
+
+function ChatBox({ chat, currentUser }) {
+  const [userData, setUserData] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  useEffect(() => {
+    const userId = chat?.members?.find((id) => id !== currentUser);
+    const getuserData = async () => {
+      try {
+        const { data } = await getUser(userId);
+        setUserData(data);
+        // console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (chat !== null) getuserData();
+  }, [chat, currentUser]);
+
+  //fetching messages
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        const { data } = await getMessages(chat._id);
+        console.log(data);
+        setMessages(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (chat !== null) fetchMessages();
+  }, [chat]);
+
+  const handleChange = (newMessage) => {
+    setNewMessage(newMessage);
+  };
+
+  return (
+    <>
+      <div className="ChatBox-container">
+        {chat ? (
+          <>
+            <div className="chat-header">
+              <div className="follower">
+                <div>
+                  <img
+                    src={
+                      userData?.profilePicture
+                        ? process.env.REACT_APP_PUBLIC_FOLDER +
+                          userData.profilePicture
+                        : process.env.REACT_APP_PUBLIC_FOLDER +
+                          "defaultProfile.png"
+                    }
+                    alt=""
+                    className="followerImage"
+                    style={{ width: "50px", height: "50px" }}
+                  />
+                  <div className="name" style={{ fontSize: "0.8rem" }}>
+                    <span>
+                      {userData?.firstname} {userData?.lastname}
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <hr style={{ width: "85%", border: "0.1px solid #ececec" }} />
+            </div>
+
+            {/* chatbox messages */}
+            <div className="chat-body">
+              {messages.map((message) => (
+                <>
+                  <div
+                    className={
+                      message.senderId === currentUser
+                        ? "message own"
+                        : "message"
+                    }
+                  >
+                    <span>{message.text}</span>
+                    <span>{format(message.createdAt)}</span>
+                  </div>
+                </>
+              ))}
+            </div>
+            {/* chat sender */}
+            <div className="chat-sender">
+              <div>+</div>
+              <InputEmoji value={newMessage} onChange={handleChange} />
+              <div className="send-button button">Send</div>
+            </div>
+          </>
+        ) : (
+          <span className="chatbox-empty-message">Tap on a chat</span>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default ChatBox;
