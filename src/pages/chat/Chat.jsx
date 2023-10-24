4dbdb97f42026aa8499a04025d225e1b8b6ef120
Dhiraj <dkskp2005@gmail.com>
2023-10-24 19:47:25 +0530

    socket client to server connection and messages

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Chat.css";
 import LogoSearch from "../../components/logoSearch/LogoSearch";
 import { useSelector } from "react-redux";
@@ -12,23 +12,62 @@ import Comment from "../../img/comment.png";
 import { UilSetting } from "@iconscout/react-unicons";
 import ChatBox from "../../components/chatBox/ChatBox";
 
+import { io } from "socket.io-client";
+
 function Chat() {
   const { user } = useSelector((state) => state.authReducer.authData);
   const [chats, setChats] = useState([]);
   const [currentChat, setCurrentChat] = useState(null);
 
+  const socket = useRef();
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [sendMessage, setSendMessage] = useState(null);
+  const [receivedMessage, setReceivedMessage] = useState(null);
+
+  //get chat in chat section
   useEffect(() => {
     const getChats = async () => {
       try {
         const { data } = await userChats(user._id);
         setChats(data);
-        console.log(data);
+        // console.log(data);
       } catch (error) {
         console.log(error);
       }
     };
     getChats();
+  }, [user._id]);
+
+  //connect to socket.io server
+  useEffect(() => {
+    socket.current = io("http://localhost:8800");
+    socket.current.emit("new-user-add", user._id);
+    socket.current.on("get-users", (users) => {
+      setOnlineUsers(users);
+      // console.log(users);
+    });
   }, [user]);
+
+  // sending message to socket server
+  useEffect(() => {
+    if (sendMessage !== null) {
+      socket.current.emit("send-message", sendMessage);
+    }
+  }, [sendMessage]);
+
+  //receive message from socket server
+  useEffect(() => {
+    socket.current.on("receive-message", (data) => {
+      console.log("Data received in parent chat.jsx", data);
+      setReceivedMessage(data);
+    });
+  }, []);
+
+  const checkOnlineStatus = (chat) => {
+    const chatMembers = chat.members.find((memeber) => memeber !== user._id);
+    const online = onlineUsers.find((user) => user.userId === chatMembers);
+    return online ? true : false;
+  };
 
   return (
     <div className="Chat">
@@ -40,7 +79,11 @@ function Chat() {
           <div className="Chat-list">
             {chats.map((chat) => (
               <div onClick={() => setCurrentChat(chat)}>
-                <Conversation data={chat} currentUserId={user._id} />
+                <Conversation
+                  data={chat}
+                  currentUserId={user._id}
+                  online={checkOnlineStatus(chat)}
+                />
               </div>
             ))}
           </div>
@@ -59,8 +102,12 @@ function Chat() {
           </Link>
         </div>
         {/* chat body */}
-        <ChatBox chat={currentChat} currentUser={user._id} />
-        {/* 59:00 */}
+        <ChatBox
+          chat={currentChat}
+          currentUser={user._id}
+          setSendMessage={setSendMessage}
+          receivedMessage={receivedMessage}
+        />
       </div>
     </div>
   );
